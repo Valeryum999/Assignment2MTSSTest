816ba9f01219172eadcb884d30c24eaa0e8c3ab4
Valeryum999 <belenkov33@gmail.com>
2024-05-14 15:15:00 +0200

    added classes letsgoooo

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////
+// Valeri Mihail Belenkov 2068232
+// Matteo Piron 2076044
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert(int number){
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};  
+        String[] romanLetters = {"M","CM","D","CD","C",
+                                "XC","L","XL","X","IX",
+                                "V","IV","I"};  
+        String result = "";
+        for(int i=0;i<values.length;i++){  
+            while(number >= values[i]){  
+                number = number - values[i];  
+                result += romanLetters[i];
+            }  
+        }  
+        return result;
+    }
+}
+
