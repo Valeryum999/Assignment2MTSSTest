189e6de117f82f4ebdb383ea62c0d2303270404e
Valeryum999 <belenkov33@gmail.com>
2024-05-14 13:16:18 +0200

    hope and pray

---
+++ src/test/java/it/unipd/mtss/InputTest.java
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// Valeri Mihail Belenkov 2068232
+// Matteo Piron 2076044
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class InputTest {
+    
+    @Test
+    public void BasicTest() {
+        assert(1 == 1);
+    }
+}
